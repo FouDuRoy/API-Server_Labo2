9867bd692205cca2564d97fb122d24d6fcdeb8a2
Olivier Provost <olivierprovost1@outlook.com>
2022-09-26 19:52:15 -0400

    Change title for message

--- models/bookmark.js
+++ models/bookmark.js
@@ -1,14 +1,14 @@
 const Model = require('./model');
 module.exports =
     class Bookmark extends Model {
-        constructor(title, url, category) {
+        constructor(name, url, category) {
             super();
-            this.Title = title !== undefined ? title : "";
+            this.Name = name !== undefined ? name : "";
             this.Url = url !== undefined ? url : "";
             this.Category = category !== undefined ? category : "";
 
             this.setKey("Url");
-            this.addValidator('Title', 'string');
+            this.addValidator('Name', 'string');
             this.addValidator('Url', 'url');
             this.addValidator('Category', 'string');
         }
